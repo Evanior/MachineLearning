093cf3cb0f0b87bd5a4d4decce8e657e86a689c6
tim <tim@Tim-PC.wat.hegyd.net>
2018-05-16 10:24:56 +0200

    test back

--- projet_photo/routes/index.js
+++ projet_photo/routes/index.js
@@ -1,9 +1,56 @@
 var express = require('express');
 var router = express.Router();
 
+var mongodb = require('mongodb');
+var urlMongo = "mongodb://127.0.0.1:27017/";
+
+var mongoClient = mongodb.MongoClient;
+
+var fs = require('fs');
+var CsvReadableStream = require('csv-reader');
+
+var inputStream = fs.createReadStream('db_photo_urls.csv', 'utf8');
+var images = {images:[]};
+
+inputStream
+  .pipe(CsvReadableStream({ parseNumbers: true, parseBooleans: true, trim: true }))
+  .on('data', function (row) {
+    /*mongoClient.connect(urlMongo, function(err, client) {
+    	if(err) {
+    		console.log("Impossible de se connecter à mongoDB.Erreur:", err);
+    	} else {
+    		var db = client.db('projet_photo');
+    		console.log("connection OK");
+        var collection = db.collection('photos');
+        var photo = {
+    				_id: row[0],
+    				url: row[1],
+    			};
+    			collection.insert(photo, function(err, result) {
+    				if(err){
+    					console.log(err);
+    				} else {
+    					console.log(photo, "done");
+    				}
+    			});
+        client.close();
+      }
+    });*/
+    images.images[row[0]] = row[1];
+  })
+  .on('end', function (data) {
+    console.log('No more rows!');
+  });
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //console.log(images);
+  res.render('index', { title: 'Index', images: images });
+});
+
+router.get('/photo', function(req, res, next){
+  console.log(images);
+  res.json(images);
 });
 
 module.exports = router;
